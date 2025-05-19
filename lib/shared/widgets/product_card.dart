a48919acc30f8aaec0db087203f22e757463bfdf
Layanga-Rajapakshe <dimutech46@gmail.com>
2025-05-19 12:23:04 +0530

    Created product card widget

--- lib/shared/widgets/product_card.dart
+++ lib/shared/widgets/product_card.dart
@@ -13,9 +13,54 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double cardWidth = screenWidth * 0.45; // 40% of the screen width
+
     return Card(
+      color: Colors.white,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
+        side: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+          width: 0.4
+        ),
+      ),
+      elevation: 0,
+      child: SizedBox(
+        width: cardWidth,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: screenWidth * 0.04,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "\$"+price.toString(),
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
