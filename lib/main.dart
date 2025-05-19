c581cc69b6137314a375c57f953a40bcff500d56
Layanga-Rajapakshe <dimutech46@gmail.com>
2025-05-19 13:12:55 +0530

    Added poppins font

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'Poppins',
         primaryColor: AppColors.primaryColor,
         primaryColorDark: AppColors.darkPrimaryColor,
         primaryColorLight: AppColors.lightPrimaryColor,
