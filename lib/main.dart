85ca2eb32e04712ba32edba42c9d75ae52cc0316
Layanga-Rajapakshe <dimutech46@gmail.com>
2025-05-20 12:05:10 +0530

    Cart page UI finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopfinity/features/auth/login_screen.dart';
+import 'package:shopfinity/features/product/cart_screen.dart';
 import 'package:shopfinity/features/product/product_screen.dart';
 import 'package:shopfinity/shared/widgets/button.dart';
 import 'package:shopfinity/shared/widgets/cart_card.dart';
@@ -30,6 +31,9 @@ class MyApp extends StatelessWidget {
           bodySmall: TextStyle(color: AppColors.secondaryText), 
         ),
         scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+        ),
         colorScheme: ColorScheme.fromSeed(
           seedColor: AppColors.primaryColor,
           secondary: AppColors.accentColor,
@@ -61,18 +65,6 @@ class _MyHomePageState extends State<MyHomePage> {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
 
-        return Center(
-          child: Container(
-            height: screenHeight * 0.22,
-            width: screenWidth * 0.9,
-            child: CartCard(
-              title: "Essence Mascara Lash Princess", 
-              imageUrl: "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/thumbnail.webp", 
-              price: 9.99, 
-              quantity: 5, 
-              description: "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
-            ),
-          ),
-        );
+        return CartScreen();
   }
 }
