16f7a50dd431842d8fd087104062f8cceffa73ce
Layanga-Rajapakshe <dimutech46@gmail.com>
2025-05-21 15:37:15 +0530

    Created login function

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shopfinity/features/product/home_screen.dart';
+import 'package:http/http.dart' as http;
 
 class LoginController extends GetxController{
   final username = TextEditingController();
@@ -17,7 +20,31 @@ class LoginController extends GetxController{
   void login() async {
   if (formKey.currentState!.validate()) {
     isLoading.value = true;
-    await Future.delayed(Duration(seconds: 2)); 
+    
+    try {
+      final response = await http.post(
+        Uri.parse("'https://dummyjson.com/auth/login'"),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(({
+          'username': username.text,
+          'password': password.text,
+        })),
+      );
+
+      if (response.statusCode == 200) {
+
+      }else{
+        final error = jsonDecode(response.body);
+        Get.snackbar(error, "Invalid username or password", snackPosition: SnackPosition.BOTTOM);
+      }
+
+    } catch (e) {
+      Get.snackbar("Error", "An error occurred: $e", snackPosition: SnackPosition.BOTTOM);
+    } finally {
+      isLoading.value = false;
+    }
 
     isLoading.value = false;
     Get.snackbar("Success", "Logged in successfully", snackPosition: SnackPosition.BOTTOM);
--- lib/features/auth/login_screen.dart
+++ lib/features/auth/login_screen.dart
@@ -14,84 +14,82 @@ class LoginPage extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Form(
-          key: controller.formKey,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "lib/assets/images/login_page_logo.png",
-                  height: screenHeight * 0.1,
+        body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+        key: controller.formKey,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "lib/assets/images/login_page_logo.png",
+                height: screenHeight * 0.1,
+              ),
+              Text(
+                "Welcome Back",
+                style: TextStyle(
+                  fontSize: screenHeight * 0.04,
                 ),
-                Text(
-                  "Welcome Back",
-                  style: TextStyle(
-                    fontSize: screenHeight * 0.04,
-                  ),
+              ),
+              SizedBox(
+                height: screenHeight * 0.08,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person_2_outlined),
+                  labelText: "Username",
                 ),
-                SizedBox(
-                  height: screenHeight * 0.08,
-                ),
+                controller: controller.username,
+                validator: (value) {
+                  if (value == null || value.isEmpty){
+                    return "*Please enter username";
+                  }else{
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(
+                height: screenHeight * 0.02,
+              ),
+              Obx(() => 
                 TextFormField(
+                  obscureText: controller.isPasswordVisible.value ? false : true,
                   decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.person_2_outlined),
-                    labelText: "Username",
+                    prefixIcon: const Icon(Icons.lock_outline),
+                    labelText: "Password",
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        controller.isPasswordVisible.value
+                            ? Icons.visibility_outlined
+                            : Icons.visibility_off_outlined,
+                        ),
+                      onPressed: () {
+                        controller.togglePasswordVisibility();
+                      },
+                    ),
                   ),
-                  controller: controller.username,
+                  controller: controller.password,
                   validator: (value) {
                     if (value == null || value.isEmpty){
-                      return "*Please enter username";
+                      return "*Please enter password";
                     }else{
                       return null;
                     }
                   },
                 ),
-                SizedBox(
-                  height: screenHeight * 0.02,
-                ),
-                Obx(() => 
-                  TextFormField(
-                    obscureText: controller.isPasswordVisible.value ? false : true,
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.lock_outline),
-                      labelText: "Password",
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          controller.isPasswordVisible.value
-                              ? Icons.visibility_outlined
-                              : Icons.visibility_off_outlined,
-                          ),
-                        onPressed: () {
-                          controller.togglePasswordVisibility();
-                        },
-                      ),
-                    ),
-                    controller: controller.password,
-                    validator: (value) {
-                      if (value == null || value.isEmpty){
-                        return "*Please enter password";
-                      }else{
-                        return null;
-                      }
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: screenHeight * 0.08,
-                ),
-                Button(
-                    text: "Login",
-                    onPressed: controller.login
-                )
-              ],
-            ),
-                ),
-                ),
-        ),
+              ),
+              SizedBox(
+                height: screenHeight * 0.08,
+              ),
+              Button(
+                  text: "Login",
+                  onPressed: controller.login
+              )
+            ],
+          ),
+              ),
+              ),
     );
   }
 }
