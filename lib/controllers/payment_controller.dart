1da3aa05d7a405a556bcee1b195929735c68129c
Layanga-Rajapakshe <dimutech46@gmail.com>
2025-05-23 12:46:34 +0530

    Added form validations

--- lib/controllers/payment_controller.dart
+++ lib/controllers/payment_controller.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class PaymentController extends GetxController {
   var paymentStatus = 'Pending'.obs;
+  final formkey = GlobalKey<FormState>();
 
   void processPayment() {
 
